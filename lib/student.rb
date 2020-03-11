93f18e19a5fd248717655aab53327f500a436658
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-11 21:25:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,12 +75,11 @@ class Student
     SELECT *
     FROM students 
     WHERE grade = 10 
-    LIMIT 1 
     SQL
     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end 
+    end.first 
   end 
   
   def save
