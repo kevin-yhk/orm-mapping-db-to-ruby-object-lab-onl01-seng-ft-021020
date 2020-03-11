496271f43197f5dbdcaca5d01a92cdcfbcb7a09d
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-11 21:20:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,13 @@ class Student
     end 
   end 
   
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10 
+    LIMIT 
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
